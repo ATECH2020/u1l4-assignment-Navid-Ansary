d0a206e8b706d35b43384b149a4cedb2f65c9da2
Navid Ansary <navid.203721@nv.ccsd.net>
2020-09-07 03:13:13 +0000

    Completed Timestamps

--- src/main/java/Timestamps.java
+++ src/main/java/Timestamps.java
@@ -3,22 +3,57 @@ import java.util.Scanner;
 class Timestamps {
     public static void main(String[] args) {
 
-        //Creates a new scanner for user input
+       //Creates a new scanner for user input
         Scanner scanner = new Scanner(System.in);
-
+        
         //Reads ints from user
-        int hours1 = scanner.nextInt();
-        int minutes1 = scanner.nextInt();
-        int seconds1 = scanner.nextInt();
-        int hours2 = scanner.nextInt();
-        int minutes2 = scanner.nextInt();
-        int seconds2 = scanner.nextInt();
+        System.out.print("Hour 1: ");
+        int H1 = scanner.nextInt(); // Hour 1
+        
+        System.out.print("Minute 1: ");
+        int M1 = scanner.nextInt(); // Minute 1
+        
+        System.out.print("Seconds 1: ");
+        int S1 = scanner.nextInt(); // Second 1
+        
+        System.out.print("\nHour 2: ");
+        int H2 = scanner.nextInt(); // Hour 2
+        
+        System.out.print("Minute 2: ");
+        int M2 = scanner.nextInt(); // Minute 2
+        
+        System.out.print("Seconds 2: ");
+        int S2 = scanner.nextInt(); // Second 2
 
+        int HS = 3600; // Seconds in an Hour
+        int MS = 60; // Seconds in a Minute
 
-        /*
-         *  your code goes here
-         */
+        int DH; // Delta Hour
+        int DM; // Delta Minute
+        int DS; // Delta Second
 
+        int SDH; // Delta Hour converted to Seconds
+        int SDM; // Delta Minute convereted to Seconds
+
+        int total;
+
+        //---your code goes here----
+        DH = H2 - H1;
+        DM = M2 - M1;
+        DS = S2 - S1;
+
+        //All Delta Differences have been calculated
+
+        SDH = DH * HS;
+        SDM = DM * MS;
+
+        // All Delta Factors have been calculated
+
+        total = SDH + SDM + DS;
+
+        //Total has been calculated
+
+        System.out.println("\nTotal Seconds = " + total);
 
         // closing the scanner object
         scanner.close();
